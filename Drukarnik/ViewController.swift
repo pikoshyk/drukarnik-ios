bd5006fd97fb75c3b6027135eb27906b2127c271
Logout <logout@unrestricted>
2022-11-29 12:41:19 +0300

    Initial Commit

---
+++ Drukarnik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drukarnik
+//
+//  Created by Logout on 29.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
