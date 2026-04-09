3c9d9f089938238b703a9caebe0dcfc161e3ed8c
Logout <logout@unrestricted>
2026-04-09 16:21:18 +0300

    Updated localization Updated provisioning profiles

--- Drukarnik/ScreenDictionary/DKWordModel+DictionaryType.swift
+++ Drukarnik/ScreenDictionary/DKWordModel+DictionaryType.swift
@@ -10,8 +10,8 @@ import Foundation
 extension DKWordModel {
     var dictionaryName: String? {
         switch self.dictionaryType {
-        case .rus: "рус"
-        case .bel: "бел"
+        case .rus: String("рус")
+        case .bel: String("бел")
         case .unknown: nil
         }
     }
